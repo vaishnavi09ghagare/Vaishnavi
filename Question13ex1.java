ae2a2f017fa9186c27450478b9a4e18ef46230be
vaishnavi09ghagare <97271522+vaishnavi09ghagare@users.noreply.github.com>
2022-01-28 09:44:06 +0530

    Add files via upload

---
+++ Question13ex1.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class Question13ex1
+{
+    public static void main(String[] args)
+    {
+        // Create a new Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the number of rows from the user
+        System.out.println("Enter the number of rows to print the pattern ");
+
+        int rows = scanner.nextInt();
+        System.out.println("Printing the pattern !!");
+        
+        for (int i=1; i<=rows; i++) 
+        { 
+            // Print space in decreasing order 
+            for (int j=rows; j>i; j--)
+            {
+                System.out.print(" ");
+            }
+            // Print star in increasing order
+            for (int k=1; k<=(i * 2) -1; k++) 
+            { 
+               System.out.print("*"); 
+            } 
+            System.out.println(); 
+        } 
+        for (int i=rows-1; i>=1; i--)
+        {
+         // Print space in increasing order
+            for (int j=rows-1; j>=i; j--)
+            {
+                System.out.print(" ");
+            }
+            // Print star in decreasing order
+            for (int k=1; k<=(i * 2) -1; k++)
+            {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
